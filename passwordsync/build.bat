a912d39133acc5648335ad8dc1c620e9779b1d29
Rich Megginson <rmeggins@redhat.com>
2009-11-10 07:57:56 -0700

    Fix install issues on 2008 - bump version to 1.1.3 Install was failing when the server could not be started * service was being installed in interactive mode - 2008 doesn't like this * cannot wait for service to start - just continue with installation

--- passwordsync/build.bat
+++ passwordsync/build.bat
@@ -156,7 +156,7 @@ if [%BRANDNOSPACE%] == [] (
    set BRANDNOSPACE=389
 )
 if [%VERSION%] == [] (
-   set VERSION=1.1.2
+   set VERSION=1.1.3
 )
 
 rem ======== Set Various Build Directories ========
